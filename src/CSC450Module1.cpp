51adf87981ae9cbf07db11f029e2590790971ef0
Maynard Peralta <maynperalta@gmail.com>
2026-09-18 00:59:59 -0600

    Fixed source code problems

--- src/CSC450Module1.cpp
+++ src/CSC450Module1.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!! Are you ready?" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!! This is a test." << endl; // prints !!!Hello World!!!
 	return 0;
 }
