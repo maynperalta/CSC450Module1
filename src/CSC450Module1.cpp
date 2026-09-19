a859821aebf3ef79d81a86279e5ad594f0b1efec
Maynard Peralta <maynperalta@gmail.com>
2026-09-18 23:24:21 -0600

    Added completed assignment files.

--- src/CSC450Module1.cpp
+++ src/CSC450Module1.cpp
@@ -1,15 +1,20 @@
-//============================================================================
-// Name        : CSC450Module1.cpp
-// Author      : MP
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!! This is a test." << endl; // prints !!!Hello World!!!
+// Store the personal information of a fictional character
+	string firstName = "Todd";
+	string lastName = "Walker";
+	string streetAddress = "12345 Main Street";
+	string city = "Denver";
+	string state = "Colorado";
+	string zipCode = "80205";
+// Display the personal information of a fictional character
+	cout << "Name: " << lastName << ", " << firstName << endl;
+	cout << "Address: " << streetAddress << endl;
+	cout << "City: " << city << endl;
+	cout << "State: " << state << endl;
+	cout << "Zip Code: " << zipCode << endl;
+
 	return 0;
 }
---
+++ src/CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,20 @@
+/* Simple Program with a few Errors for Correction
+   Please be sure to correct all outlined errors.
+*/
+
+#include <iostream>
+// #include <conio.h>  // Not needed for this program. Causes error on MacOS
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+    // Standard Output Statement
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+
+    // Main Function return Statement
+    return 0;
+}
